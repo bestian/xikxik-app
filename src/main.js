5ad143c89ef9235596caf14e098dce63853a0e14
Bestian Tang <bestian@gmail.com>
2019-06-12 05:39:14 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 import '../node_modules/semantic-ui-css/semantic.min.css'
 
 Vue.config.productionTip = false
